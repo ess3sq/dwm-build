7895309619086f4fbd64a4d279db43672b7d189a
Lorenzo Calza <lorenzo@lorenzocalza.com>
2022-06-02 19:33:50 +0200

    Change to SF font(s), name tags and add tag switching for emacs and web

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 48;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
-static const char *fonts[]          = { "monospace:size=14", "Symbola:size=14" };
+static const char *fonts[]          = { "SF Mono:style=Light:size=14", "monospace:size=14", "Symbola:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -19,7 +19,13 @@ static const char col_cyan[]        = "#005577";
 // Custom colors: https://www.color-hex.com/color/00539b
 static const char col_oceanblue[]     = "#00539b";
 static const char col_draculapurple[] = "#6272A4";
-static const char * const col_main = col_oceanblue;
+static const char col_charcoal[]      = "#2E4052";
+static const char col_phtalogreen[]   = "#264027";
+static const char col_oxfordblue[]    = "#0B2545";
+static const char col_bloodred[]      = "#5A1807";
+static const char col_ocrered[]       = "#55251D";
+static const char col_spaceblue[]     = "#283044";
+static const char * const col_main = col_oxfordblue;
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -29,22 +35,26 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "⛺", "2", "3", "🎧", "🎮", "💬", "📂", "💻", "🌐" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                  instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",				  NULL,       NULL,       0,            1,           -1 },
-	{ "Emacs",				  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "Firefox",			  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Brave",				  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "jetbrains-idea",		  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "jetbrains-clion",	  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "Thunderbird",	      NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Discord",     	      NULL,       NULL,       1 << 6,       0,           -1 },
+	/* class                  instance    title               tags mask     isfloating   monitor */
+	{ "Gimp",				  NULL,       NULL,               0,            1,           -1 },
+	{ "Emacs",				  NULL,       NULL,               1 << 7,       0,           -1 },
+	{ "Firefox",			  NULL,       NULL,               1 << 8,       0,           -1 },
+	{ "Brave",				  NULL,       NULL,               1 << 8,       0,           -1 },
+	{ "jetbrains-idea",		  NULL,       NULL,               1 << 7,       0,           -1 },
+	{ "jetbrains-clion",	  NULL,       NULL,               1 << 7,       0,           -1 },
+	{ "Thunderbird",	      NULL,       NULL,               1 << 5,       0,           -1 },
+	{ "discord",     	      NULL,       NULL,               1 << 5,       0,           -1 },
+	{ NULL,          	      NULL,       "Spotify",          1 << 5,       0,           -1 },
+	{ "org.remmina.Remmina",  NULL,       "Remmina Remote",   0,            0,           -1 }, // remote machine selection
+	//	{ "org.remmina.Remmina",  NULL,       "Remmina",          1 << 1,       0,           -1 }, // remote machine window
+	{ "org.remmina.Remmina",  NULL,       "newraider",        1 << 1,       0,           -1 }, // newraider window
 };
 
 /* layout(s) */
@@ -80,6 +90,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 // static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *configscmd[] = {"config-edit.sh", NULL};
 
 // Custom apps/commands/hotkeys
 // static const char  *firefoxcmd[] = { "/usr/bin/firefox",  NULL };
@@ -95,7 +106,11 @@ static const char  *screenshotcmd[] = { "/usr/bin/gnome-screenshot",  "-i", NULL
 static const char  *thunderbirdcmd[] = { "/usr/bin/thunderbird", NULL };
 
 static const char  *slockcmd[] = { "/usr/local/bin/slock",  NULL };
+static const char  *systemcmd[] = { "/home/lorenzo/bin/system-control",  NULL };
+static const char  *suspendcmd[] = {"systemctl", "suspend", NULL };
 static const char  *shutdowncmd[] = { "/home/lorenzo/bin/shutdown-now",  NULL };
+static const char  *gamescmd[] = {"game-launcher.sh", NULL};
+static const char  *docscmd[] = {"open_doc.sh", NULL};
 
 static const char  *volumelevelcmd[] = { "python3",  "/home/lorenzo/bin/volume_level.py", NULL };
 static const char  *volumeup[] = { "amixer",  "set", "Master", "3+", NULL };
@@ -108,10 +123,38 @@ static const char  *brightmax[] = { "xbacklight",  "-set", "100",  NULL };
 
 static const char  *wifistrengthcmd[] = { "python3",  "/home/lorenzo/bin/wifi_strength.py", NULL};
 
+void suspend(const Arg *nullarg) {
+	Arg slock = {.v = slockcmd};
+	Arg suspend = {.v = suspendcmd};
+	spawn(&slock);
+	spawn(&suspend);
+}
+
+void switch_to_tag(const Arg *arg) {
+	// {.ui = 1 << TAG}
+	view(arg);
+	toggleview(arg);
+	tag(arg);
+	toggletag(arg);
+}
+
+void open_emacs(const Arg *arg) {
+	Arg emacsarg = {.v = emacscmd};
+	spawn(&emacsarg);
+	switch_to_tag(arg);
+}
+
+void open_web(const Arg *arg) {
+	Arg bravearg = {.v = bravecmd};
+	spawn(&bravearg);
+	switch_to_tag(arg);
+}
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = configscmd } },
+ 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
@@ -127,7 +170,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 
-	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_x,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -150,15 +193,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_q,      spawn,          {.v = systemcmd} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 
 	// Apps
 	//	{ MODKEY,                       XK_w,      spawn,          {.v = firefoxcmd} },
-	{ MODKEY,                       XK_w,      spawn,          {.v = bravecmd} },
+	{ MODKEY,                       XK_w,      open_web,       {.ui = 1 << 8} },
 	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = surfcmd} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = surfbookcmd} },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = surfhistcmd} },
-	{ MODKEY,                       XK_e,      spawn,          {.v = emacscmd} },
+	{ MODKEY,                       XK_e,      open_emacs,     {.ui = 1 << 7} },
 	{ MODKEY,                       XK_o,      spawn,          {.v = nautiluscmd} },
 	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = screenshotcmd} },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = thunderbirdcmd} },
@@ -166,9 +210,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_v,      spawn,          {.v = volumelevelcmd} },
 	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = wifistrengthcmd} },
 
-	{ MODKEY,                       XK_F1,     spawn,          {.v = slockcmd} },
+	{ MODKEY,                       XK_F1,     suspend,        {.v = NULL} },
 	{ MODKEY|ShiftMask,             XK_F1,     spawn,          {.v = shutdowncmd} },
-
+	{ MODKEY,                       XK_g,      spawn,          {.v = gamescmd} },
+	{ MODKEY|ShiftMask,             XK_g,      spawn,          {.v = docscmd} },
+	
 	{ MODKEY,                       XK_F12,    spawn,          {.v = volumeup} },
 	{ MODKEY,                       XK_F11,    spawn,          {.v = volumedown} },
 	{ MODKEY,                       XK_F10,    spawn,          {.v = volumetoggle} },
