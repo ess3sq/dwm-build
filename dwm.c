b00ad33fe933f99fa5927cefecf0bc23054773ff
Lorenzo Calza <lorenzo@lorenzocalza.com>
2023-06-09 00:07:19 +0200

    Make icontray follow sidepad and vertpad values for bar

--- dwm.c
+++ dwm.c
@@ -2522,9 +2522,9 @@ updatesystray(void)
 			i->mon = m;
 	}
 	w = w ? w + systrayspacing : 1;
-	x -= w;
-	XMoveResizeWindow(dpy, systray->win, x, m->by, w, bh);
-	wc.x = x; wc.y = m->by; wc.width = w; wc.height = bh;
+	x -= w + sidepad;
+	XMoveResizeWindow(dpy, systray->win, x, m->by + vertpad, w, bh);
+	wc.x = x; wc.y = m->by + vertpad; wc.width = w; wc.height = bh;
 	wc.stack_mode = Above; wc.sibling = m->barwin;
 	XConfigureWindow(dpy, systray->win, CWX|CWY|CWWidth|CWHeight|CWSibling|CWStackMode, &wc);
 	XMapWindow(dpy, systray->win);
